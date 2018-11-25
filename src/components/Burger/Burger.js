40cf23f2d1023114c8f81307b1024e200912b8f2
pskanade <k.pranav.apk@gmail.com>
2018-11-26 01:10:48 +0530

    Building burger dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,16 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 export default props => {
+  const fillingIngredients = Object.keys(props.ingredients).map(fKey => {
+    return [...Array(props.ingredients[fKey])].map((_, i) => {
+      return <BurgerIngredient key={fKey + i} type={fKey} />;
+    });
+  });
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {fillingIngredients}
       <BurgerIngredient type="bread-bottom" />
-      <p>This is ingredient div</p>
     </div>
   );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,12 +3,19 @@ import Aux from "../../hoc/Aux";
 import Burger from "./../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      cheese: 2,
+      bacon: 1,
+      salad: 1,
+      meat: 2
+    }
+  };
+
   render() {
     return (
       <Aux>
-        <div>This is just before the top of the burger</div>
-        <Burger />
-        <div>Tool to add Burger Ingredients</div>
+        <Burger ingredients={this.state.ingredients} />
       </Aux>
     );
   }
